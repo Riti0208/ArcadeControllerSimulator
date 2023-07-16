bad3f863dd790681b63973bf8ed6f97280bd495f
Riti0208 <daichizaki@yahoo.co.jp>
2023-07-16 12:29:45 +0900

    change unity file name

--- src/components/UnityArcadeControllerSimulator.tsx
+++ src/components/UnityArcadeControllerSimulator.tsx
@@ -2,10 +2,12 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const UnityArcadeControllerSimulator = () => {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "ArcadeController/Build/Dist.loader.js",
-    dataUrl: "ArcadeController/Build/Dist.data",
-    frameworkUrl: "ArcadeController/Build/Dist.framework.js",
-    codeUrl: "ArcadeController/Build/Dist.wasm",
+    loaderUrl:
+      "ArcadeController/Build/ArcadeControllerSimulator.data.loader.js",
+    dataUrl: "ArcadeController/Build/ArcadeControllerSimulator.data.data",
+    frameworkUrl:
+      "ArcadeController/Build/ArcadeControllerSimulator.data.framework.js",
+    codeUrl: "ArcadeController/Build/ArcadeControllerSimulator.data.wasm",
   });
 
   return <Unity unityProvider={unityProvider} />;
